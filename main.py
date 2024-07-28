5956190d7773956962821d28f2696a921baf9319
Marwan-Mamdouh <marwanabdelmajjed@gmail.com.com>
2024-07-28 20:39:57 +0300

    remake some functions and fix some of the structure of the app

--- main.py
+++ main.py
@@ -1,52 +1,23 @@
 import users
-import TODOlists
+import todo_app
 
 print('Hello, this is my TODO list app')
 
 
 def main():
     """
-    take no arg 
+    take no arg
 
     produce all the app to run
     """
-    status = input(
-        'Type "N" if you want to register Or "O" if you have a list... ').lower().strip()
+    status = input('Type "N" if you want to register Or "O"'
+                   'if you have a list... ').lower().strip()
     if status != 'n' and status != 'o':
         print('Invalid input')
         main()
     else:
         userName = input('Type your username... ').lower().strip()
-        check_user(userName, status)
-
-
-def check_user(userName, status):
-    """
-    string, char -> None
-
-    take the username and the status of the user to 
-    return the list of tasks or add a new task
-    """
-    found = userName in users.get_users()
-    if found and status == 'o':
-        print(f'Hello, {userName}')
-        TODOlists.make_choice(userName)
-
-    elif status == 'o':
-        print(f'username not found.')
-        check_user(input('try again... '), status)
-
-    elif found and status == 'n':
-        print(f'Sorry, this username is already taken. try other username')
-        check_user(input('... '), status)
-
-    elif not found and status == 'n':
-        users.new_user(userName)
-        print(f'Your username has been add to the database.')
-        TODOlists.make_choice(userName)
-    else:
-        print('Invalid input\ntry again')
-        main()
+        users.check_user(userName, status)
 
 
 main()
--- users.py
+++ users.py
@@ -1,9 +1,39 @@
+import todo_app
 # user functions file
 
 
-def get_users() -> list[str]:
+def check_user(userName, status):
+    """string, char -> None
+
+    take the username and the status of the user to 
+    return the list of tasks or add a new task
     """
-    take no arg
+    found = userName in get_users()
+
+    if found and status == 'o':
+        print(f'Hello, {userName}')
+        todo_app.make_choice(userName)
+
+    elif status == 'o':
+        print(f'username not found.')
+        check_user(input('try again... '), status)
+
+    elif found and status == 'n':
+        print(f'Sorry, this username is already taken. try other username')
+        check_user(input('... '), status)
+
+    elif not found and status == 'n':
+        new_user(userName)
+        print(f'Your username has been add to the database.')
+        todo_app.make_choice(userName)
+    else:
+        print('Invalid input\ntry again')
+        check_user(input('Inter your user name ...').lower().strip(),
+                   input('O for old user, N for new user').lower().strip())
+
+
+def get_users() -> list[str]:
+    """take no arg
 
     return list of users (string)
     """
@@ -26,7 +56,7 @@ def new_user(username: str) -> None:
         data.write(f'{username},')
 
     with open(f'./database/{username}.txt', 'w') as newFile:
-        newFile.write(f'Hello this {username} tasks file')
+        newFile.write(f'\nHello this {username} tasks file')
 
 # for test do not need this now
 # print(new_user('mohamed'))
